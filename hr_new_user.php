9508c19cb1376989d253d2268f9271af93bbadbd
githubUserNo123 <alif.numan@sphiere.com.bn>
2017-05-24 10:56:40 +0800

    Modified form attributes to match the input names

--- hr_new_user.php
+++ hr_new_user.php
@@ -12,15 +12,35 @@
 
 	<section class="content">
 		<div class="box">
-			<div class="box-header">
-				 <h3 class="box-title">Personal Information</h3>
-			</div>
-			<form name="new_sheet" method="POST" action="<?php echo $current_file; ?>">
+
+			<?php if( !empty( $_POST["submit"] ) ) : ?>
+				<?php echo '<pre>' . var_export($_POST, true) . '</pre>'; ?>
+			<?php endif; ?>
+
+			<form id="new_sheet" name="new_sheet" method="POST">
+				<div class="box-header">
+					 <h3 class="box-title">Personal Information</h3>
+				</div>
+
 				<div class="box-body">
 					<div class = "col-md-6">
 						<div class="form-group">
-							<label for="exampleInputEmail1">Full Name</label>
-							<input type="text" class="form-control" placeholder="Enter full name" name="fullname">
+							<label for="user_fullname">Full Name</label>
+							<input id="user_fullname" name="user_fullname" type="text" class="form-control" placeholder="Enter full name" required>
+						</div>
+					</div>
+
+					<div class = "col-md-6">
+						<div class="form-group">
+							<label for="user_email">Email</label>
+							<input id="user_email" name="user_email" type="email" class="form-control" placeholder="Enter email" required>
+						</div>
+					</div>
+
+					<div class = "col-md-6">
+						<div class="form-group">
+							<label for="user_ic">IC</label>
+							<input id="user_ic" name="user_ic" type="text" class="form-control" placeholder="Enter ic" required>
 						</div>
 					</div>
 				</div><!-- /.box-body -->
@@ -32,66 +52,48 @@
 				<div class="box-body">
 					<div class="col-md-6">
 						<div class="form-group">
-							<label for="exampleInputEmail1">Employment Type</label>
-							<select class="form-control" >
-								<option>--Please Select--</option>
-								<?php $sql = mysql_query("select * from usertemplevel"); ?>
-									
-								<?php while($row = mysql_fetch_array($sql)) : ?>
-									<option><?php echo $row['UserTempLevel_name']?></option>
-								<?php endwhile; ?>
+							<label for="user_type">Employment Type</label>
+							<select id="user_type" name="user_type" class="form-control" >
+								<option value="0">Temporary</option>
+								<option value="1">Permanent</option>
 							</select>
 						</div>
 					</div><!-- /.col-md-6 -->
 
 					<div class = "col-md-6">
 						<div class="form-group">
-							<label for="exampleInputEmail1">Daily basis salary</label>
+							<label for="user_salary">Daily salary</label>
 							<div class="input-group">
 								<span class="input-group-addon">$</span>
-								<input type="text" class="form-control" placeholder="xxx.xx" />
+								<input id="user_salary" name="user_salary" type="number" step="0.1" min="1" max="500" class="form-control" placeholder="xxx.xx" required>
 							</div>
 						</div>
 					</div><!-- /.col-md-6 -->
 				</div><!-- /.box-body -->
 
 				<div class="box-header">
-					<h3 class="box-title">Account Setting</h3>
+					<h3 class="box-title">Account Settings</h3>
 				</div>
 
 				<div class="box-body">
 					<div class="col-md-6">
 						<div class="form-group">
-							<label for="exampleInputEmail1">User name</label>
-							<input type="text" class="form-control" placeholder="Enter full name">
+							<label for="user_username">Username</label>
+							<input id="user_username" name="user_username" type="text" class="form-control" placeholder="Enter username" required>
 						</div>
 					</div><!-- /.col-md-6 -->
 
 					<div class="col-md-6">
 						<div class="form-group">
-							<label for="exampleInputEmail1">Assign password</label>
-							<input type="text" class="form-control" placeholder="Enter full name">
-						</div>
-					</div><!-- /.col-md-6 -->
-				</div><!-- /.box-body -->
-
-				<div class="box-header">
-					<h3 class="box-title">Security Access Pass ID</h3>
-				</div>
-
-				<div class="box-body">
-					<div class="col-md-6">
-						<div class="form-group">
-							<label for="exampleInputEmail1">Pass ID Number</label><br/>
-							<i>Pass ID number is located at the behind of the physical pass ID</i>
-							<input type="text" class="form-control" placeholder="0000:00000">
+							<label for="user_password">Password</label>
+							<input id="user_password" name="user_password" type="text" class="form-control" placeholder="Enter password" required>
 						</div>
 					</div><!-- /.col-md-6 -->
 				</div><!-- /.box-body -->
 
 				<div class="box-footer">
 					<div class="pull-right">
-						<a type="button" class="btn btn-purple">Create User</a>
+						<button name="submit" value="1" class="btn btn-purple" type="submit">Create User</button>
 					</div>
 				</div><!-- /.box-footer -->
 			</form>
